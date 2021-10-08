50106c970869c98032dc2f0924d1b648f9ca720c
mitriifd <idimka9@gmail.com>
2021-10-08 14:31:27 +0500

    fadIn fadeOut

---
+++ Создаем свою JavaScript UI библиотеку/Создаем анимации fadeIn_fadeOut/src/js/main.js
@@ -0,0 +1,17 @@
+import $ from './lib/lib';
+
+$('button').on('click', function () {
+    $('div').eq(1).toggleClass('active');
+});
+
+$('div').click(function () {
+    console.log($(this).index());
+});
+
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findme'));
+console.log($('.findme').siblings());
+
+$('.findme').fadeIn(1800);
+
+// console.log($('button').html('Hello'));
